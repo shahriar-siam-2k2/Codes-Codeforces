1d0231ba6df9c6453b3ad959f2294bfb987e3036
Shahriar Ali Siam <shahriaraliseam@gmail.com>
2023-09-26 11:59:32 +0600

    225212347-ACCEPTED

--- Problemset/199A-Hexadecimal's theorem/199A-HexaTheo.cpp
+++ Problemset/199A-Hexadecimal's theorem/199A-HexaTheo.cpp
@@ -1,34 +1,9 @@
 #include <iostream>
 using namespace std;
-
+ 
 int main(){
-    long long int n;
+    long long n;
     cin >> n;
-    long long int f[n+2];
-
-    f[0] = 0, f[1] = 1;
-    for(int i=2 ; i<=n+1 ; i++){
-        f[i] = f[i-2] + f[i-1];
-    }
-
-    // for(int i=0 ; i<=n+1 ; i++){
-    //     cout << f[i] << endl;
-    // }
-
-    for(int i=0 ; i<=n ; i++){
-        for(int j=i ; j<=n ; j++){
-            for(int k=j ; k<=n ; k++){
-                int sum = f[i] + f[j] + f[k];
-                if(sum == n){
-                    cout << f[i] << " " << f[j] << " " << f[k];
-                    return 0;
-                }
-                else if(sum > n){
-                    continue;
-                }
-            }
-        }
-    }
-    cout << "I'm too stupid to solve this problem";
+    cout << 0 << " " << 0 << " " << n;
     return 0;
 }
